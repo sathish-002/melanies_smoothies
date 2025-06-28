a3fd2089ddce73f15cc7434a8b8ef831d488b96d
Sathish Vadivel <117635296+sathish-002@users.noreply.github.com>
2025-06-28 17:51:40 +0530

    Update streamlit_app.py
    session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title('My Parents New Healthy Diner')
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
 st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie:')
